d6b59692dda7e0d5e87ecb363d3701aed6284158
Htet Wia Aung <htetwai343@gmail.com>
2026-02-05 16:46:00 +0630

    update customer balance in POS.

--- src/stores/useStockTransactionStore.js
+++ src/stores/useStockTransactionStore.js
@@ -14,10 +14,10 @@ export const useStockTransactionStore = defineStore('Stock Transaction', {
     actions: {
         async fetchStockTransactions(filteredData) {
             this.list = [],
-            this.loading = true;
+                this.loading = true;
             this.error = [];
             try {
-                const response = await axios.get(`/stock_transactions?start_date=${filteredData.start_date}&end_date=${filteredData.end_date}&${filteredData.reference_type? reference_type = filteredData.reference_type: ""}`);
+                const response = await axios.get(`/stock_transactions?start_date=${filteredData.start_date}&end_date=${filteredData.end_date}&${filteredData.reference_type ? reference_type = filteredData.reference_type : ""}`);
                 this.list = response.data.data;
             } catch (err) {
                 this.error = normalizeApiError(err);
